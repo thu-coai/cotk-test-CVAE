e39124fc3ade27531ef17f58be3108ac4d299cee
ZhihongShao <zhshaothu@gmail.com>
2019-07-10 09:30:53 +0800

    update

--- cotk/wordvector/__init__.py
+++
@@ -1,9 +0,0 @@
-r"""
-`cotk.wordvector` provides classes and functions downloading and
-loading wordvector automatically.
-"""
-
-from .wordvector import WordVector
-from .gloves import Glove
-
-__all__ = ["WordVector", "Glove"]
